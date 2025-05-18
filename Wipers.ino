0e1da053f1ca4e14d6bbb03439015cd7d45e3e73
andreea.olteanu3110 <andreea.olteanu3110@stud.acs.upb.ro>
2025-05-18 21:35:46 +0300

    Testare LED-uri

--- Wipers.ino
+++ Wipers.ino
@@ -1,16 +1,46 @@
+int red = 7;
+int yellow = 8;
+int green = 9;
+int white = 10;
+
+int servo1 = 5;
+int servo2 = 6;
+
 void setup() {
   Serial.begin(9600);
+  pinMode(red, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(white, OUTPUT);
+
   delay(20);
 }
 
 void loop() {
-  
+  digitalWrite(white, HIGH);
+
   int rain = analogRead(A0);
   int light = analogRead(A1);
 
+  // LED-uri
+  if (rain > 950 && light < 250) {
+    digitalWrite(green, HIGH);
+    digitalWrite(yellow, LOW);
+    digitalWrite(red, LOW);
+  } else if (rain < 950 && light < 250) {
+    digitalWrite(green, LOW);
+    digitalWrite(yellow, HIGH);
+    digitalWrite(red, LOW);
+  } else if (light > 250) {
+    digitalWrite(green, LOW);
+    digitalWrite(yellow, LOW);
+    digitalWrite(red, HIGH);
+  }
+
   Serial.println(rain);
-  Serial.println(light)
+  Serial.println(light);
   delay(2000);
 
 }
+
 

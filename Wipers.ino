0fde1479239f17c59cbca76032d9f321dd2fb0d5
andreea.olteanu3110 <andreea.olteanu3110@stud.acs.upb.ro>
2025-05-18 23:41:36 +0300

    Adaugare servomotoare

--- Wipers.ino
+++ Wipers.ino
@@ -1,5 +1,17 @@
+int red = 7;
+int yellow = 8;
+int green = 9;
+int white = 10;
+
+int servo1 = 5;
+int servo2 = 6;
+
 void setup() {
   Serial.begin(9600);
+  pinMode(red, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(white, OUTPUT);
 
   delay(20);
 }
@@ -10,10 +22,56 @@ void loop() {
   int rain = analogRead(A0);
   int light = analogRead(A1);
 
-  Serial.println(rain);
-  Serial.println(light);
+  // LED-uri
+  if (rain > 950 && light < 250) {
+    digitalWrite(green, HIGH);
+    digitalWrite(yellow, LOW);
+    digitalWrite(red, LOW);
+  } else if (rain < 950 && light < 250) {
+    digitalWrite(green, LOW);
+    digitalWrite(yellow, HIGH);
+    digitalWrite(red, LOW);
+  } else if (light > 250) {
+    digitalWrite(green, LOW);
+    digitalWrite(yellow, LOW);
+    digitalWrite(red, HIGH);
+  }
+
+  // Daca ploua si parbrizul e curat:
+  if (rain < 950 && light < 250) {
+
+    int times;
+    if (rain > 800) {
+      times = 1200;
+    } else if (rain > 500) {
+      times = 600;
+    } else {
+      times = 200;
+    }
+
+    pwmServo(servo1, 600);
+    pwmServo(servo2, 600);
+    delay(300);
+
+    pwmServo(servo1, 1200);
+    pwmServo(servo2, 1200);
+    delay(times);
+
+  } else {
+    pwmServo(servo1, 1200);
+    pwmServo(servo2, 1200);
+    delay(20);
+  }
+
   delay(2000);
 
 }
 
 
+// Semnal PWM manual pentru servomotor
+void pwmServo(int pin, int pulseWidthMicros) {
+  digitalWrite(pin, HIGH);
+  delayMicroseconds(pulseWidthMicros);
+  digitalWrite(pin, LOW);
+  delay(20 - (pulseWidthMicros / 1000));
+}
